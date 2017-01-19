3079ad788f98d9303529dae19847913df823a489
Corotata <corotata@qq.com>
2017-01-19 11:56:26 +0800

    test

--- Learn Swift.playground/Pages/1.String.xcplaygroundpage/Contents.swift
+++ Learn Swift.playground/Pages/1.String.xcplaygroundpage/Contents.swift
@@ -81,10 +81,4 @@ let corotata2 = urlString.substring(with: Range(startIndex..<endIndex))
 let comIndex = urlString.index(urlString.endIndex, offsetBy: -3)
 let com2 = urlString.substring(from: comIndex)
 
-
-
-
-
-
-
 //: [Next](@next)
